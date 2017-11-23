a8b97e79f8f5c48f556f40391634589886b696a4
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-11-24 06:01:48 +0700

    Add promisify

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const google = require('googleapis');
+const Promise = require('bluebird');
 const config = require('../.config');
 
-const plus = google.plus('v1');
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(config.google.client_id, config.google.client_secret, '');
 
+const getToken = Promise.promisify(oauth2Client.getToken, {context: oauth2Client});
+const people = Promise.promisify(google.plus('v1').people.get, { context: google });
+
 router.post('/login', function(req, res, next) {
-  return oauth2Client.getToken(req.body.auth_code, (err, tokens) => {
-    if(err) {
-      console.log(err);
-      res.send({
-        status: 'failed'
-      });
-      return;
-    }
+  return getToken(req.body.auth_code)
+  .then((tokens) => {
     oauth2Client.setCredentials(tokens);
-    return plus.people.get({
+    return people({
         userId: 'me',
         auth: oauth2Client,
-      }, (errr, result) => {
-        if(errr) {
-          res.send({
-            status: 'failed'
-          });
-          return;
-        }
-        console.log(result);
-        res.send({
-          status: 'success'
-        })
       });
+  })
+  .then((result) => {
+    console.log(result);
+    res.send({ status: 'success' })
+  })
+  .catch(err => {
+    console.log(err);
+    res.send({ status: 'failed' });
   });
 });
 
