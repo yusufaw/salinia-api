92f22d72826186e3d37c4b7377ea20771759917d
Yusuf Aji Wibowo <yusuf@skyshi.com>
2018-02-04 08:03:47 +0700

    Refactor user router code

--- routes/users.js
+++ routes/users.js
@@ -1,31 +1,30 @@
 const express = require('express');
-const router = express.Router();
 const google = require('googleapis');
 const Promise = require('bluebird');
 
-const OAuth2 = google.auth.OAuth2;
+const router = express.Router();
+
+const { OAuth2 } = google.auth;
 const oauth2Client = new OAuth2(process.env.SALINIA_GOOGLE_CLIENT_ID, process.env.SALINIA_GOOGLE_CLIENT_SECRET, '');
 
-const getToken = Promise.promisify(oauth2Client.getToken, {context: oauth2Client});
+const getToken = Promise.promisify(oauth2Client.getToken, { context: oauth2Client });
 const people = Promise.promisify(google.plus('v1').people.get, { context: google });
 
-router.post('/login', function(req, res, next) {
-  return getToken(req.body.auth_code)
-  .then((tokens) => {
-    oauth2Client.setCredentials(tokens);
-    return people({
+router.post('/login', (req, res, next) => {
+  const token = getToken(req.body.auth_code)
+    .then((tokens) => {
+      oauth2Client.setCredentials(tokens);
+      return people({
         userId: 'me',
         auth: oauth2Client,
       });
-  })
-  .then((result) => {
-    console.log('image url', result.image.url.substring(0, result.image.url.lastIndexOf('.') + 4));
-    res.send({ status: 'success' })
-  })
-  .catch(err => {
-    console.log(err);
-    res.send({ status: 'failed' });
-  });
+    })
+    .then((result) => {
+      console.log('image url', result.image.url.substring(0, result.image.url.lastIndexOf('.') + 4));
+      res.send({ status: 'success' });
+    })
+    .catch(() => res.send({ status: 'failed' }));
+  return token;
 });
 
 module.exports = router;
